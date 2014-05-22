0aea1e6d06120977b2ac10be60104256a5f9c610
James Ritchie <james.a.ritchie@gmail.com>
2014-05-22 14:33:17 +0100

    Initial commit

---
+++ Processors/WormTracker.py
@@ -0,0 +1,296 @@
+from datetime import datetime
+import cPickle as pickle
+
+import numpy as np
+from scipy import interpolate
+from scipy import optimize
+
+import cv2
+
+
+class Tracker():
+    """Class for worm tracking algorithm"""
+
+    def __init__(self, microscope):
+        """Initializes the class with serial interface/camera names,
+        algorithm control parameters.
+
+        Arguments:
+        serial_port -- String of name for the serial interface for the
+            microscope Arduino - e.g. 'dev/tty.usb'
+        """
+
+        self.microscope = microscope
+        self.microscope.set_ring_colour('FF0000')
+        self.last_step_time = datetime.now()
+        self.start = datetime.now()
+
+        self.worm_spline = np.zeros((1001, 1, 2), dtype=np.int)
+        self.tail = np.zeros((2,), dtype=np.int)
+        self.head = np.zeros((2,), dtype=np.int)
+
+        self.smoothing = 100
+        self.draw_contour = True
+
+        self.tracking_log = open('tracking.pickle', 'ab')
+
+        self.contour_log = open('contour.pickle', 'ab')
+        self.skeleton_log = open('skeleton.pickle', 'ab')
+
+    def find_worm(self):
+        """Threshold and contouring algorithm to find centroid of worm"""
+        self.img_gray = cv2.cvtColor(self.img, cv2.COLOR_BGR2GRAY)
+
+        if self.params['adaptive']:
+            size = self.params['adaptive_kernel']*2 + 1
+
+            if self.params['adaptive_gauss']:
+                mode = cv2.ADAPTIVE_THRESH_MEAN_C
+            else:
+                mode = cv2.ADAPTIVE_THRESH_GAUSSIAN_C
+
+            self.img_thresh = cv2.adaptiveThreshold(self.img_gray, 255,
+                                                    mode,
+                                                    cv2.THRESH_BINARY_INV,
+                                                    size,
+                                                    self.params['adaptive_m'])
+
+        else:
+            #Threshold
+            ret, self.img_thresh = cv2.threshold(self.img_gray,
+                                                 self.params['simple_value'],
+                                                 255, cv2.THRESH_BINARY_INV)
+
+        #Perform Morphological Opening/Closing
+
+        ksize = self.params['morph_kernel']*2 + 1
+        self.kernel = np.ones((ksize, ksize), np.uint8)
+
+        if self.params['opening'] != 0:
+            self.img_thresh = cv2.morphologyEx(self.img_thresh,
+                                               cv2.MORPH_OPEN,
+                                               self.kernel,
+                                               iterations=self.params['opening'])
+
+        if self.params['closing'] != 0:
+            self.img_thresh = cv2.morphologyEx(self.img_thresh,
+                                               cv2.MORPH_CLOSE,
+                                               self.kernel,
+                                               iterations=self.params['closing'])
+
+        #Copy image to allow displaying later
+        img_contour = self.img_thresh.copy()
+        contours, hierarchy = cv2.findContours(img_contour, cv2.RETR_TREE,
+                                               cv2.CHAIN_APPROX_NONE)
+
+        #Find the biggest contour
+        worm_area = 0
+        for contour in contours:
+            area = cv2.contourArea(contour)
+            if area > worm_area:
+                self.worm = contour
+                worm_area = area
+
+        #Compute the centroid of the worm contour
+        moments = cv2.moments(self.worm)
+        self.x = int(moments['m10']/moments['m00'])
+        self.y = int(moments['m01']/moments['m00'])
+
+        #Write centroids to file
+
+        if self.record:
+            row = ('centroid', self.time, self.x, self.y)
+            pickle.dump(row, self.tracking_log)
+            pickle.dump((self.time, self.worm), self.contour_log)
+
+    def skeletonise(self):
+        #X and Y points defining worm boundary
+        x_in = self.worm[:, 0, 0]
+        y_in = self.worm[:, 0, 1]
+
+        #Fit spline to boundary
+        tck, u = interpolate.splprep([x_in, y_in], per=True, s=self.params['smoothing'],
+                                     quiet=True)
+
+        points = np.arange(0, 1.001, 0.001)
+
+        spline = interpolate.splev(points, tck, der=0)
+        x = spline[0]
+        y = spline[1]
+        self.worm_spline[:, 0, 0] = x
+        self.worm_spline[:, 0, 1] = y
+
+        d = interpolate.splev(points, tck, der=1)
+        dx = d[0]
+        dy = d[1]
+
+        dd = interpolate.splev(points, tck, der=2)
+        ddx = dd[0]
+        ddy = dd[1]
+
+        k = dx*ddy - dy*ddx
+        k = np.absolute(k)
+        k = k/((dx**2 + dy**2)**1.5)
+
+        tail_n = np.argmax(k)
+        k[tail_n-125:tail_n+125] = 0
+        head_n = np.argmax(k)
+
+        self.tail[0] = int(x[tail_n])
+        self.tail[1] = int(y[tail_n])
+
+        self.head[0] = int(x[head_n])
+        self.head[1] = int(y[head_n])
+
+        tail_p = points[tail_n]
+        head_p = points[head_n]
+
+        if head_n > tail_n:
+            u_a = np.linspace(tail_p, head_p, self.params['centre_points'])
+            side_a = interpolate.splev(u_a, tck, der=0)
+
+            n = round((1-head_p)*self.params['centre_points'])
+            u_b_1 = np.linspace(head_p, 1, n)
+            u_b_2 = np.linspace(0, tail_p, (self.params['centre_points']-n))
+            u_b = np.concatenate((u_b_1, u_b_2))
+            side_b = interpolate.splev(u_b, tck, der=0)
+            side_b = np.fliplr(side_b)
+
+        else:
+            u_a = np.linspace(head_p, tail_p, self.params['centre_points'])
+            side_a = interpolate.splev(u_a, tck, der=0)
+            side_a = np.fliplr(side_a)
+
+            n = round((1-tail_p)*self.params['centre_points'])
+            u_b_1 = np.linspace(tail_p, 1, n)
+            u_b_2 = np.linspace(0, head_p, (self.params['centre_points']-n))
+            u_b = np.concatenate((u_b_1, u_b_2))
+            side_b = interpolate.splev(u_b, tck, der=0)
+
+        self.skeleton = (side_a + side_b)/2
+
+        if self.params['refine']:
+            spline_a, u_a = interpolate.splprep(side_a, s=100)
+            spline_b, u_b = interpolate.splprep(side_b, s=100)
+            centre_spline, u = interpolate.splprep(self.skeleton, s=100)
+
+            points = np.linspace(0, 1, self.params['centre_points'])
+            self.skeleton = np.array(interpolate.splev(points, centre_spline,
+                                     der=0))
+            tangents = interpolate.splev(points, centre_spline, der=1)
+            tangents = np.array(tangents)
+            tangents = tangents/np.linalg.norm(tangents, axis=0)
+            normals = np.array([tangents[1, :]*-1, tangents[0, :]])
+
+            args = (spline_a, self.skeleton, normals)
+            sol_a = optimize.root(self.intersection, u_a,
+                                  args)
+            side_a = interpolate.splev(sol_a.x, spline_a, der=0)
+
+            args = (spline_b, self.skeleton, normals)
+            sol_b = optimize.root(self.intersection, u_b, args)
+            side_b = interpolate.splev(sol_b.x, spline_b, der=0)
+
+            self.skeleton = (np.array(side_a) + np.array(side_b))/2
+
+        if self.record:
+            pickle.dump((self.time, self.skeleton), self.skeleton_log)
+
+    def intersection(self, p, spline, origins, normals):
+
+        spline_points = np.array(interpolate.splev(p, spline, der=0))
+
+        delta = origins - spline_points
+        displacement = delta - (np.sum(delta*normals, axis=0))*normals
+
+        return displacement[0, :]
+
+    def move_stage(self):
+        now = datetime.now()
+        elapsed_time = ((now - self.last_step_time).microseconds)/1000
+
+        if self.params['tracking'] and elapsed_time > self.params['step_interval']:
+            self.last_step_time = now
+            if self.x < self.params['margin']:
+                self.microscope.move('x', -1*self.params['step_size'])
+                print 'Moving Left'
+                if self.record:
+                    row = ('step', self.time, 'x', -1)
+                    pickle.dump(row, self.tracking_log)
+
+            if self.x > (self.width - self.params['margin']):
+                self.microscope.move('x', self.params['step_size'])
+                print 'Moving Right'
+                if self.record:
+                    row = ('step', self.time, 'x', 1)
+                    pickle.dump(row, self.tracking_log)
+
+            if self.y < self.params['margin']:
+                self.microscope.move('y', -1*self.params['step_size'])
+                print 'Moving Up'
+                if self.record:
+                    row = ('step', self.time, 'y', -1)
+                    pickle.dump(row, self.tracking_log)
+
+            if self.y > (self.height - self.params['margin']):
+                self.microscope.move('y', self.params['step_size'])
+                print 'Moving Down'
+                if self.record:
+                    row = ('step', self.time, 'y', 1)
+                    pickle.dump(row, self.tracking_log)
+
+    def draw_features(self):
+
+        if self.params['show_threshold']:
+            self.img = cv2.cvtColor(self.img_thresh, cv2.COLOR_GRAY2BGR)
+
+        #Draw the splined contour if computed, otherwise
+        if self.draw_contour:
+            if self.params['skeleton']:
+                cv2.drawContours(self.img, [self.worm_spline],
+                                 -1, (255, 0, 0), 2)
+            else:
+                cv2.drawContours(self.img, [self.worm], -1, (255, 0, 0), 2)
+
+        #Draw markers for centroid and boundry
+        cv2.circle(self.img, (self.x, self.y), 5, (0, 0, 255), -1)
+
+        cv2.rectangle(self.img, (self.params['margin'], self.params['margin']),
+                      (self.width-self.params['margin'],
+                       self.height-self.params['margin']),
+                      (0, 255, 0),
+                      2)
+
+        #If skeletonise draw markers for head and tail
+        if self.params['skeleton']:
+            cv2.circle(self.img, (self.tail[0], self.tail[1]),
+                       5, (0, 255, 255), -1)
+            cv2.circle(self.img, (self.head[0], self.head[1]),
+                       5, (255, 255, 0), -1)
+
+            self.skeleton = np.transpose(self.skeleton)
+
+            cv2.polylines(self.img, np.int32([self.skeleton]),
+                          False, (255, 0, 0), 2)
+
+    def process_frame(self, img):
+
+            self.record = self.params['record']
+
+            self.time = datetime.now()
+            self.img = img
+
+            height, width, depth = self.img.shape
+
+            self.width = width
+            self.height = height
+
+            self.find_worm()
+
+            if self.params['skeleton']:
+                self.skeletonise()
+
+            self.draw_features()
+            self.move_stage()
+
+            return self.img
---
+++ client.py
@@ -0,0 +1,374 @@
+import sys
+import time
+import cv2
+
+from PySide import QtCore
+from PySide import QtGui
+
+from Processors.WormTracker import Tracker
+from interface import Microscope
+
+
+class FrameDisplay(QtGui.QLabel):
+
+    def __init__(self):
+        super(FrameDisplay, self).__init__()
+
+    @QtCore.Slot()
+    def updateFrame(self, pixmap):
+        self.setPixmap(pixmap)
+
+
+class FrameThread(QtCore.QThread):
+    '''Grabs and processes camera frames and signals when ready'''
+
+    def __init__(self, microscope):
+        super(FrameThread, self).__init__()
+        self.camera = cv2.VideoCapture(0)
+
+        self.tracker = Tracker(microscope)
+        self.mutex = QtCore.QMutex()
+        self.abort = False
+        self.condition = QtCore.QWaitCondition()
+
+    def update_parameters(self, params):
+        self.mutex.lock()
+        self.params = params
+        self.mutex.unlock()
+
+    def run(self):
+        while not self.abort:
+            self.mutex.lock()
+            self.tracker.params = self.params
+            self.mutex.unlock()
+
+            #ret, self.img = self.camera.read()
+            self.img = cv2.imread('tracking_fig01.jpg')
+            time.sleep(0.25)
+
+            self.img = self.tracker.process_frame(self.img)
+
+            self.mutex.lock()
+            self.img_copy = self.img.copy()
+            self.mutex.unlock()
+
+            height, width, channels = self.img_copy.shape
+            bpl = width*channels
+            RGBimg = cv2.cvtColor(self.img_copy, cv2.COLOR_BGR2RGB)
+            self.frame = QtGui.QImage(RGBimg.data, width, height, bpl,
+                                      QtGui.QImage.Format_RGB888)
+            self.emit(QtCore.SIGNAL("renderedImage(const QImage)"), self.frame)
+
+    def stop(self):
+        self.mutex.lock()
+        self.abort = True
+        self.condition.wakeOne()
+        self.mutex.unlock()
+
+        self.wait()
+
+
+class MicroscopeClient(QtGui.QWidget):
+    '''Application for controlling the OpenLabTools Microscope'''
+
+    newPixmap = QtCore.Signal(QtGui.QPixmap)
+
+    def __init__(self, serial_port):
+        super(MicroscopeClient, self).__init__()
+
+        self.microscope = Microscope(serial_port)
+        self.microscope.set_ring_colour('FF0000')
+
+        self.init_ui()
+
+        self.params = {}
+        self.thread = FrameThread(self.microscope)
+
+        self.set_defaults()
+
+        self.newPixmap.connect(self.frame_display.updateFrame)
+
+        #Start the frame processing thread
+        self.connect(self.thread, QtCore.SIGNAL("renderedImage(const QImage)"),
+                     self.update_frame)
+        self.thread.start()
+
+    def init_ui(self):
+        '''Define UI Elements'''
+
+        self.setWindowTitle('OpenLabTools Microscope')
+
+        layout = QtGui.QHBoxLayout()
+
+        self.frame_display = FrameDisplay()
+
+        left_column = QtGui.QVBoxLayout()
+        left_column.addWidget(self.frame_display)
+        left_column.addWidget(self.init_stage_controls())
+        left_column.addStretch(1)
+
+        right_column = QtGui.QVBoxLayout()
+        right_column.addWidget(self.init_frame_controls())
+
+        layout.addLayout(left_column)
+        layout.addLayout(right_column)
+        self.setLayout(layout)
+
+    def init_stage_controls(self):
+        '''Define UI Elements for controlling the stage'''
+
+        group_box = QtGui.QGroupBox('Stage Controls')
+
+        hbox = QtGui.QHBoxLayout()
+
+        grid = QtGui.QGridLayout()
+
+        up_button = QtGui.QToolButton()
+        up_button.setArrowType(QtCore.Qt.ArrowType.UpArrow)
+        down_button = QtGui.QToolButton()
+        down_button.setArrowType(QtCore.Qt.ArrowType.DownArrow)
+        left_button = QtGui.QToolButton()
+        left_button.setArrowType(QtCore.Qt.ArrowType.LeftArrow)
+        right_button = QtGui.QToolButton()
+        right_button.setArrowType(QtCore.Qt.ArrowType.RightArrow)
+
+        grid.addWidget(up_button, 0, 1)
+        grid.addWidget(down_button, 2, 1)
+        grid.addWidget(left_button, 1, 0)
+        grid.addWidget(right_button, 1, 2)
+
+        xy_group = QtGui.QGroupBox('XY Controls')
+        xy_group.setLayout(grid)
+
+        hbox.addWidget(xy_group)
+
+        z_group = QtGui.QGroupBox('Z Control')
+
+        z_up_button = QtGui.QToolButton()
+        z_down_button = QtGui.QToolButton()
+
+        z_up_button.setArrowType(QtCore.Qt.ArrowType.UpArrow)
+        z_down_button.setArrowType(QtCore.Qt.ArrowType.DownArrow)
+
+        vbox = QtGui.QVBoxLayout()
+        vbox.addWidget(z_up_button)
+        vbox.addStretch(1)
+        vbox.addWidget(z_down_button)
+
+        z_group.setLayout(vbox)
+
+        hbox.addWidget(z_group)
+
+        hbox.addStretch(1)
+
+        group_box.setLayout(hbox)
+
+        return group_box
+
+    def init_frame_controls(self):
+        '''Define UI Elements for changing image processing parameters'''
+
+        group_box = QtGui.QGroupBox('Frame Controls')
+
+        vbox = QtGui.QVBoxLayout()
+
+        #Thresholding Parameters
+
+        thresh_group = QtGui.QGroupBox('Thresholding')
+
+        thresh_form = QtGui.QFormLayout()
+
+        self.simple_radio = QtGui.QRadioButton('Simple')
+        self.simple_radio.toggled.connect(self.update_parameters)
+        self.adapt_radio = QtGui.QRadioButton('Adaptive')
+        self.adapt_radio.toggled.connect(self.update_parameters)
+
+        thresh_mode = QtGui.QVBoxLayout()
+        thresh_mode.addWidget(self.simple_radio)
+        thresh_mode.addWidget(self.adapt_radio)
+
+        thresh_mode_group = QtGui.QGroupBox()
+        thresh_mode_group.setLayout(thresh_mode)
+
+        thresh_form.addRow('Mode', thresh_mode_group)
+
+        self.simple_thresh_value = QtGui.QSlider(QtCore.Qt.Horizontal)
+        self.simple_thresh_value.valueChanged.connect(self.update_parameters)
+        thresh_form.addRow('Simple Threshold', self.simple_thresh_value)
+
+        self.mean_radio = QtGui.QRadioButton('Mean')
+        self.mean_radio.toggled.connect(self.update_parameters)
+        self.gauss_radio = QtGui.QRadioButton('Gaussian')
+        self.gauss_radio.toggled.connect(self.update_parameters)
+
+        adapt_kernel = QtGui.QVBoxLayout()
+        adapt_kernel.addWidget(self.mean_radio)
+        adapt_kernel.addWidget(self.gauss_radio)
+        adapt_kernel_group = QtGui.QGroupBox()
+        adapt_kernel_group.setLayout(adapt_kernel)
+        thresh_form.addRow('Adaptive Kernel', adapt_kernel_group)
+
+        self.adapt_kernel_size = QtGui.QSpinBox()
+        self.adapt_kernel_size.valueChanged.connect(self.update_parameters)
+        thresh_form.addRow('Kernel Size', self.adapt_kernel_size)
+
+        self.adapt_kernel_mean = QtGui.QSlider(QtCore.Qt.Horizontal)
+        self.adapt_kernel_mean.valueChanged.connect(self.update_parameters)
+        thresh_form.addRow('Adaptive Mean', self.adapt_kernel_mean)
+
+        self.morph_open = QtGui.QSpinBox()
+        self.morph_open.valueChanged.connect(self.update_parameters)
+        thresh_form.addRow('Opening Rounds', self.morph_open)
+
+        self.morph_close = QtGui.QSpinBox()
+        self.morph_close.valueChanged.connect(self.update_parameters)
+        thresh_form.addRow('Closing Rounds', self.morph_close)
+
+        self.morph_kernel = QtGui.QSpinBox()
+        self.morph_kernel.valueChanged.connect(self.update_parameters)
+        thresh_form.addRow('Morphological Kernel Size', self.morph_kernel)
+
+        self.show_thresh = QtGui.QCheckBox()
+        self.show_thresh.toggled.connect(self.update_parameters)
+        thresh_form.addRow('Show Thresholded Image', self.show_thresh)
+
+        thresh_group.setLayout(thresh_form)
+        vbox.addWidget(thresh_group)
+
+        #Tracking Parameters
+
+        tracking_group = QtGui.QGroupBox('Tracking')
+
+        tracking_form = QtGui.QFormLayout()
+
+        self.enable_tracking = QtGui.QCheckBox()
+        self.enable_tracking.toggled.connect(self.update_parameters)
+        tracking_form.addRow('Enable Tracking', self.enable_tracking)
+
+        self.step_size = QtGui.QSpinBox()
+        self.step_size.valueChanged.connect(self.update_parameters)
+        tracking_form.addRow('Step Size', self.step_size)
+
+        self.step_interval = QtGui.QSlider(QtCore.Qt.Horizontal)
+        self.step_interval.valueChanged.connect(self.update_parameters)
+        tracking_form.addRow('Step Interval', self.step_interval)
+
+        self.margin = QtGui.QSlider(QtCore.Qt.Horizontal)
+        self.margin.valueChanged.connect(self.update_parameters)
+        tracking_form.addRow('Centroid Margin', self.margin)
+
+        tracking_group.setLayout(tracking_form)
+        vbox.addWidget(tracking_group)
+
+        skeleton_group = QtGui.QGroupBox('Skeletonisation')
+        skeleton_form = QtGui.QFormLayout()
+
+        self.enable_skeleton = QtGui.QCheckBox()
+        self.enable_skeleton.toggled.connect(self.update_parameters)
+        skeleton_form.addRow('Enable Skeletonisation', self.enable_skeleton)
+
+        self.spline_smoothing = QtGui.QSlider(QtCore.Qt.Horizontal)
+        self.spline_smoothing.valueChanged.connect(self.update_parameters)
+        skeleton_form.addRow('Spline Smoothing', self.spline_smoothing)
+
+        self.centre_line = QtGui.QSlider(QtCore.Qt.Horizontal)
+        self.centre_line.valueChanged.connect(self.update_parameters)
+        skeleton_form.addRow('Centre Line Points', self.centre_line)
+
+        self.refine = QtGui.QCheckBox()
+        self.refine.toggled.connect(self.update_parameters)
+        skeleton_form.addRow('Refine Centreline', self.refine)
+
+        self.record = QtGui.QCheckBox()
+        self.record.toggled.connect(self.update_parameters)
+        skeleton_form.addRow('Record', self.record)
+
+        skeleton_group.setLayout(skeleton_form)
+        vbox.addWidget(skeleton_group)
+
+        vbox.addStretch(1)
+        group_box.setLayout(vbox)
+
+        return group_box
+
+    def set_defaults(self):
+        self.simple_radio.setChecked(True)
+        self.simple_thresh_value.setRange(0, 255)
+        self.simple_thresh_value.setValue(100)
+        self.adapt_kernel_size.setValue(1)
+        self.adapt_kernel_mean.setRange(0, 10)
+        self.adapt_kernel_mean.setValue(5)
+        self.mean_radio.setChecked(True)
+        self.show_thresh.setChecked(False)
+
+        self.enable_tracking.setChecked(False)
+        self.step_size.setValue(1)
+        self.step_interval.setRange(0, 1000)
+        self.step_interval.setValue(500)
+        self.margin.setRange(1, 200)
+        self.margin.setValue(50)
+
+        self.enable_skeleton.setChecked(False)
+        self.spline_smoothing.setRange(0, 1000)
+        self.spline_smoothing.setValue(100)
+        self.centre_line.setRange(0, 600)
+        self.centre_line.setValue(102)
+
+        self.update_parameters()
+
+    @QtCore.Slot()
+    def update_parameters(self):
+
+        #Grab parameters from UI Widgets
+        self.params['adaptive'] = self.adapt_radio.isChecked()
+        self.params['simple_value'] = self.simple_thresh_value.value()
+        self.params['adaptive_gauss'] = self.gauss_radio.isChecked()
+        self.params['adaptive_kernel'] = self.adapt_kernel_size.value()
+        self.params['adaptive_m'] = self.adapt_kernel_mean.value()
+        self.params['opening'] = self.morph_open.value()
+        self.params['closing'] = self.morph_close.value()
+        self.params['morph_kernel'] = self.morph_kernel.value()
+        self.params['show_threshold'] = self.show_thresh.isChecked()
+
+        self.params['tracking'] = self.enable_tracking.isChecked()
+        self.params['step_size'] = self.step_size.value()
+        self.params['step_interval'] = self.step_size.value()
+        self.params['margin'] = self.margin.value()
+
+        self.params['skeleton'] = self.enable_skeleton.isChecked()
+        self.params['smoothing'] = self.spline_smoothing.value()
+        self.params['centre_points'] = self.centre_line.value()
+        self.params['refine'] = self.refine.isChecked()
+        self.params['record'] = self.refine.isChecked()
+
+        #Disable thresholding params depending on mode
+        if self.params['adaptive']:
+            self.simple_thresh_value.setEnabled(False)
+            self.mean_radio.setEnabled(True)
+            self.gauss_radio.setEnabled(True)
+            self.adapt_kernel_size.setEnabled(True)
+            self.adapt_kernel_mean.setEnabled(True)
+        else:
+            self.simple_thresh_value.setEnabled(True)
+            self.mean_radio.setEnabled(False)
+            self.gauss_radio.setEnabled(False)
+            self.adapt_kernel_size.setEnabled(False)
+            self.adapt_kernel_mean.setEnabled(False)
+
+        #Signal to transfer params to processing thread
+        self.thread.update_parameters(self.params)
+
+    @QtCore.Slot()
+    def update_frame(self, frame):
+        '''Resend signal from frame thread to frame widget'''
+        self.pixmap = QtGui.QPixmap.fromImage(frame)
+        self.newPixmap.emit(self.pixmap)
+        self.update()
+
+if __name__ == "__main__":
+    app = QtGui.QApplication(sys.argv)
+    widget = MicroscopeClient(sys.argv[1])
+    widget.show()
+    r = app.exec_()
+    widget.thread.stop()
+    sys.exit(r)
